876c995ac0fe97b600ffee91ff03e0786956b2fe
Valeria <valeri.kiass.love@gmail.com>
2025-10-30 18:32:44 +0300

    feat: реализована модалка "О проекте" через React.Portal

--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { ThemeSwitcher } from "../../features/ThemeSwitcher/ui/ThemeSwitcher";
+import { Button } from "../../shared/ui/Button/Button";
+import { Modal } from "../../shared/ui/Modal/Modal";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
   return (
-  <header>
-    <h1>PostViewer</h1>
-    <ThemeSwitcher />
-  </header>
+    <header>
+      <h1>PostViewer</h1>
+      <ThemeSwitcher />
+      <Button onClick={() => setIsModalOpen(true)}>О проекте</Button>
+
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h2>О проекте</h2>
+        <p>
+          Это учебное приложение для просмотра постов.
+        </p>
+      </Modal>
+    </header>
   )
 };
 
